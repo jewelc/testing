f7ee05fa3178a5c19de8f906e46eb7f146d5dae0
Jewel Chakma <jewel.chakma@gmail.com>
2023-07-14 17:11:36 +0000

    initial commit

---
+++ testing/testing/doctype/testing/testing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jctkorp.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Testing', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ testing/testing/doctype/testing/testing.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, jctkorp.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Testing(Document):
+	pass
