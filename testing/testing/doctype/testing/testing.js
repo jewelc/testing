93551330057ef32602ce9d1e7f75847931e5ed17
Jewel Chakma <jewel.chakma@gmail.com>
2023-07-14 17:38:13 +0000

    second commit

--- testing/testing/doctype/testing/testing.js
+++ testing/testing/doctype/testing/testing.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Testing', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frappe.msgprint("Hi")
+	}
 });
